8dc041f16d52b9711b438acedcb8e6a1831cdd0f
Saverio Trioni <saverio.trioni@gmail.com>
2012-11-27 15:50:51 +0100

    wrong things in session controller (until now unused)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
 
-  user = User.find_by_email(params[:email])
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => "Logged in!"
-  else
-    flash.now.alert = "Invalid email or password"
-    render "new"
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
   end
 
 end
