240257a47c37c855495bf4382274a10b7416ace6
Saverio Trioni <saverio.trioni@gmail.com>
2013-05-15 02:19:23 +0200

    fix to be able to run migration in heroku

--- db/migrate/20130508085004_add_name_translation_to_category.rb
+++ db/migrate/20130508085004_add_name_translation_to_category.rb
@@ -1,8 +1,4 @@
 class AddNameTranslationToCategory < ActiveRecord::Migration
-  class Category < ActiveRecord::Base
-    attr_accessor :old_name
-    translates :name
-  end
   def up
     ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS hstore;")
     add_column :categories, :name_translations, :hstore
