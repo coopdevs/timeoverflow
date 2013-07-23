c4c859f1d249465fa79435a282bd6a260bccff5e
Saverio Trioni <saverio.trioni@gmail.com>
2013-07-23 12:55:34 +0200

    Added extended Persona configuration
    ENV['PERSONA_AUDIENCE'] can now contain several audiences, and the
    `authenticate` method may ask for a specific audience.

--- lib/persona.rb
+++ lib/persona.rb
@@ -1,10 +1,14 @@
 module Persona
 
-  def self.authenticate(assertion)
+  def self.authenticate(assertion, options={})
     server = 'https://verifier.login.persona.org/verify'
+    available_audience = ENV['PERSONA_AUDIENCE'].split
+    if requested_audience = options[:audience]
+      return {status: 'error'}.to_json unless available_audience.include? requested_audience
+    end
     assertion_params = {
       assertion: assertion,
-      audience: ENV['PERSONA_AUDIENCE'] || 'http://0.0.0.0:3000'
+      audience: requested_audience || available_audience.first.presence || 'http://0.0.0.0:3000'
     }
     request = RestClient::Resource.new(server, verify_ssl: true).post(assertion_params)
     response = JSON.parse(request)
