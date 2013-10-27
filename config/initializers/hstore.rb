94a075a1c2cd9cd738ab3a5658a59fe9d1fa6954
Saverio Trioni <saverio.trioni@gmail.com>
2013-10-27 22:26:55 +0100

    Hey, adding postgres extensions is supported!

--- config/initializers/hstore.rb
+++
@@ -1,5 +0,0 @@
-begin
-  ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS hstore;")
-  ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm;")
-rescue PG::ConnectionBad
-end
