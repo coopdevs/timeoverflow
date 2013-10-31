b93871ec1b092b52f94f1a83a8409532fefa3d03
Saverio Trioni <saverio.trioni@gmail.com>
2013-10-31 03:27:33 +0100

    Fix organization creation from admin.

--- app/admin/organization.rb
+++ app/admin/organization.rb
@@ -1,3 +1,26 @@
 ActiveAdmin.register Organization do
+  index do
+    id_column
+    column :name
+    column :created_at
+    column :updated_at
+    # column :email
+    # column :domain
+    default_actions
+  end
 
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :name
+      # f.input :email
+      # f.input :domain
+    end
+    f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit organization: [:name]
+    end
+  end
 end
