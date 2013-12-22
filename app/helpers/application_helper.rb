ea413da9ec3e91b58d77f08de766fb8e74136f5b
Saverio Trioni <saverio.trioni@gmail.com>
2013-12-22 19:00:04 +0100

    Update bootstrap version (includes support for center-block class)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,9 @@ module ApplicationHelper
   def theme_stylesheet_link_tag
     theme = current_organization.try(:theme)
     url = if Organization::BOOTSWATCH_THEMES.include? theme
-      "//netdna.bootstrapcdn.com/bootswatch/3.0.0/#{theme}/bootstrap.min.css"
+      "//netdna.bootstrapcdn.com/bootswatch/3.0.3/#{theme}/bootstrap.min.css"
     else
-      "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
+      "//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"
     end
     stylesheet_link_tag url
   end
