762c23746de4b0ac025dc1c056dca87323a70bec
Saverio Trioni <saverio.trioni@gmail.com>
2013-12-31 12:38:40 +0100

    User controller now uses devise for blocking unauthenticated access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
   respond_to :html, :js
 
   def scoped_users
-    return User.none unless current_user
     current_organization.users
   end
 
