c357f91938a62360f6313af8cc20b8e677f04e28
Saverio Trioni <saverio.trioni@gmail.com>
2014-01-01 11:22:40 +0100

    no need to configure smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,15 +59,15 @@ Timeoverflow::Application.configure do
     protocol: (ENV["MAIL_LINK_PROTO"] || "https")
   }
 
-  config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADRESS'],
-    port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USER'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: ENV['SMTP_ADRESS'],
+  #   port: ENV['SMTP_PORT'],
+  #   domain: ENV['SMTP_DOMAIN'],
+  #   user_name: ENV['SMTP_USER'],
+  #   password: ENV['SMTP_PASSWORD'],
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
   # Enable threaded mode
   # config.threadsafe!
