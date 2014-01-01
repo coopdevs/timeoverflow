4fbc0031322fb14f940797eba9a40e8f2156b26c
Saverio Trioni <saverio.trioni@gmail.com>
2014-01-01 20:17:57 +0100

    fix apth for activeadmin logout link

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # coding: utf-8
 
 ActiveAdmin.setup do |config|
@@ -97,7 +99,7 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :sessions_path
+  config.logout_link_path = :destroy_user_session_path
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
