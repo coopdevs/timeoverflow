03eb0b80106f32b937374c77a0a6ab1ce78fd9cf
Patri <patrimglez@gmail.com>
2014-05-09 04:41:51 +0200

    users as json

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,26 @@
 module UsersHelper
+
+  def users_as_json
+    @users.map do |user|
+    {
+      id: user.id,
+      avatar: avatar_url(user),
+      member_id: @memberships[user.id].member_uid,
+      username: user.username,
+      email: user.email,
+      phone: user.phone,
+      alt_phone: user.alt_phone,
+      balance: @memberships[user.id].account_balance.to_i,
+
+      url: user_path(user),
+      edit_link: (superadmin? || admin? || user == current_user)? edit_user_path(user) : "",
+      cancel_link: (superadmin? || admin?)? member_path(@memberships[user.id]) : "",
+      manage_link: ((superadmin? || admin?) && user != current_user)? toggle_manager_member_path(@memberships[user.id]) : "",
+      manager: @memberships[user.id].manager ||= false,
+      activate_link: (superadmin?)? user_toggle_active_path(user) : "",
+      active: user.active?
+    }
+    end.to_json.html_safe
+  end
+
 end
