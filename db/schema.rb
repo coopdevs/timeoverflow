c034df6f9a2d0387b39719709323697408d149ad
Patri <patrimglez@gmail.com>
2014-05-15 01:00:14 +0200

    add active column to members

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119161433) do
+ActiveRecord::Schema.define(version: 20140514225527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140119161433) do
     t.datetime "updated_at"
     t.date     "entry_date"
     t.integer  "member_uid"
+    t.boolean  "active",          default: true
   end
 
   add_index "members", ["organization_id"], name: "index_members_on_organization_id", using: :btree
