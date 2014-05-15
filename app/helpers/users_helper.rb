da4beacdb188eed921bd78ec70a72f69ff3d20be
Patri <patrimglez@gmail.com>
2014-05-15 03:41:15 +0200

    actions according to active/inactive user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,8 +15,8 @@ module UsersHelper
 
       url: user_path(user),
       edit_link: (superadmin? || admin? || user == current_user)? edit_user_path(user) : "",
-      cancel_link: (superadmin? || admin?)? member_path(@memberships[user.id]) : "",
-      manage_link: ((superadmin? || admin?) && user != current_user)? toggle_manager_member_path(@memberships[user.id]) : "",
+      cancel_link: ((superadmin? || admin?) && !@memberships[user.id].active?)? member_path(@memberships[user.id]) : "",
+      manage_link: ((superadmin? || admin?) && user != current_user && @memberships[user.id].active?)? toggle_manager_member_path(@memberships[user.id]) : "",
       manager: @memberships[user.id].manager ||= false,
       activate_link: (superadmin? || admin?)? toggle_active_member_path(@memberships[user.id]) : "",
       active: @memberships[user.id].active?
