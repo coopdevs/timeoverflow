ac0269ce1ac42e3584163030b01d737d3a6ffa68
Saverio Trioni <saverio.trioni@gmail.com>
2014-05-22 00:19:13 +0200

    Users helper:
    * Pon los enlaces siempre, así al cambiar de estado se puede mostrar.
    * calcula la membership solamente una vez por user
    * indent y espacios entre a ? b : c

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,24 +3,25 @@ module UsersHelper
   def users_as_json
     @users = (admin? || superadmin?)? @users : @users.actives
     @users.map do |user|
-    {
-      id: user.id,
-      avatar: avatar_url(user),
-      member_id: @memberships[user.id].member_uid,
-      username: user.username,
-      email: user.email,
-      phone: user.phone,
-      alt_phone: user.alt_phone,
-      balance: @memberships[user.id].account_balance.to_i,
+      membership = membership
+      {
+        id: user.id,
+        avatar: avatar_url(user),
+        member_id: membership.member_uid,
+        username: user.username,
+        email: user.email,
+        phone: user.phone,
+        alt_phone: user.alt_phone,
+        balance: membership.account_balance.to_i,
 
-      url: user_path(user),
-      edit_link: (superadmin? || admin? || user == current_user)? edit_user_path(user) : "",
-      cancel_link: ((superadmin? || admin?) && !@memberships[user.id].active?)? member_path(@memberships[user.id]) : "",
-      manage_link: ((superadmin? || admin?) && user != current_user && @memberships[user.id].active?)? toggle_manager_member_path(@memberships[user.id]) : "",
-      manager: @memberships[user.id].manager ||= false,
-      activate_link: (superadmin? || admin?)? toggle_active_member_path(@memberships[user.id]) : "",
-      active: @memberships[user.id].active?
-    }
+        url: user_path(user),
+        edit_link: (superadmin? || admin? || user == current_user) ? edit_user_path(user) : "",
+        cancel_link: (superadmin? || admin?) ? member_path(membership) : "",
+        toggle_manager_link: ((superadmin? || admin?) && user != current_user) ? toggle_manager_member_path(membership) : "",
+        manager: !!membership.manager,
+        toggle_active_link: (superadmin? || admin?) ? toggle_active_member_path(membership) : "",
+        active: membership.active?
+      }
     end.to_json.html_safe
   end
 
