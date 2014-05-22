b79da5687646403d0a485e7704b0eb568efb34a4
Saverio Trioni <saverio.trioni@gmail.com>
2014-05-22 10:50:59 +0200

    Post.actives is now a scope, and loads the member_id attribute in the model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,14 @@ class Post < ActiveRecord::Base
   include Taggable
   extend Searchable :title, :description
 
+  attr_reader :member_id
+
   belongs_to :category
   belongs_to :user
   belongs_to :organization
   belongs_to :publisher, :class_name => "User", :foreign_key => "publisher_id"
+
+  has_many :user_members, class_name: "Member", through: :user, source: :members
 
   has_and_belongs_to_many :joined_users,
     class_name: "User",
@@ -18,15 +22,29 @@ class Post < ActiveRecord::Base
   default_scope ->{ order('posts.updated_at DESC') }
 
   scope :by_category, ->(cat) { where(category_id: cat) if cat }
-  scope :actives, -> { select{ |p| p.organization.members.find_by_member_uid(p.member_id).active == true } }
-  
+
+  scope :with_member, -> {
+    joins(:organization, :user_members).
+    where("members.organization_id = posts.organization_id").
+    select("posts.*, members.member_uid as member_id")
+  }
+
+  scope :actives, -> {
+    with_member.merge(Member.active)
+  }
+
   validates :user, presence: true
 
   def to_s
     title
   end
 
+  # will read the member_id if it has been returned by the query, otherwise
+  # don't complain and return nil.
+  #
+  # To ensure the presence of the attribute, use the `with_member` scope
   def member_id
-    organization.members.where(user_id: user_id).take.member_uid
+    read_attribute(:member_id) if has_attribute?(:member_id)
   end
+
 end
