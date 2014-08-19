675d776f5d198ecdaa9a47faa99ad2f8eb38aa46
Xavi Vila <xavi@desktop.(none)>
2014-08-19 18:21:16 +0200

    showing offers list as a dashboard

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,13 +1,10 @@
 class OffersController < PostsController
 
   def dashboard
-    get_collection_ivar || begin
-      offers_hash = Hash.new
-      Category.all.each do |category|
-        offers = current_organization.posts.where(type: "offer".capitalize.constantize, category_id: category.id).page(1).per(5)
-        offers_hash[category] = offers if offers.any?
-      end
-      set_collection_ivar(offers_hash)
+    @offers = Hash.new
+    Category.all.each do |category|
+      offers_by_category = current_organization.offers.merge(category.posts).limit(5)
+      @offers[category] = offers_by_category if offers_by_category.any?
     end
   end
 
