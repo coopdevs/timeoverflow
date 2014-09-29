45af911164f5eef7395c89f4484efbc39c7713c3
Saverio Trioni <saverio.trioni@gmail.com>
2014-09-29 22:42:13 +0200

    Factored out login(user) from tests.

--- spec/controllers/transfers_controller_spec.rb
+++ spec/controllers/transfers_controller_spec.rb
@@ -7,29 +7,27 @@ describe TransfersController do
   let (:member_taker) { Fabricate(:member, organization: test_organization) }
 
   describe "POST #create" do
+    before { login(user) }
+
     context "with valid params" do
       context "with an admin user logged" do
+
+        let(:user) { member_admin.user }
         subject { post 'create', transfer: {source: member_giver.account.id, destination: member_taker.account.id, amount: 5} }
 
         it "creates a new Transfer" do
-          login(member_admin.user)
-
           expect {
             subject
           }.to change(Transfer, :count).by 1
         end
 
         it "creates two Movements" do
-          login(member_admin.user)
-
           expect {
             subject
           }.to change { Movement.count}.by 2
         end
 
         it "updates the balance of both accounts" do
-          login(member_admin.user)
-
           expect {
             subject
             member_giver.reload
@@ -39,32 +37,27 @@ describe TransfersController do
             subject
             member_taker.reload
           }.to change { member_taker.account.balance.to_i }.by 5
-
         end
       end
 
       context "with a regular user logged" do
+
+        let(:user) { member_giver.user }
         subject { post 'create', transfer: {destination: member_taker.account.id, amount: 5} }
 
         it "creates a new Transfer" do
-          login(member_giver.user)
-
           expect {
             subject
           }.to change(Transfer, :count).by 1
         end
 
         it "creates two Movements" do
-          login(member_giver.user)
-
           expect {
             subject
           }.to change { Movement.count}.by 2
         end
 
         it "updates the balance of both accounts" do
-          login(member_giver.user)
-
           expect {
             subject
             member_giver.reload
@@ -74,43 +67,9 @@ describe TransfersController do
             subject
             member_taker.reload
           }.to change { member_taker.account.balance.to_i }.by 5
-
         end
       end
     end
 
-    # context "with valid params" do
-    #   context "with an admin user logged" do
-    #     it "creates a new Transfer" do
-    #       login(member_admin.user)
-
-    #       post 'create', transfer: {source: member_giver.account.id, destination: member_taker.account.id, amount: 5}
-
-    #       expect(Transfer.count).to eq(1)
-    #       expect(Movement.count).to eq(2)
-
-    #       member_giver.reload
-    #       member_taker.reload
-    #       expect(member_giver.account.balance.to_i).to eq(-5)
-    #       expect(member_taker.account.balance.to_i).to eq(5)
-    #     end
-    #   end
-
-    #   context "with a regular user logged" do
-    #     it "creates a new Transfer" do
-    #       login(member_giver.user)
-
-    #       post 'create', transfer: {destination: member_taker.account.id, amount: 5}
-
-    #       expect(Transfer.count).to eq(1)
-    #       expect(Movement.count).to eq(2)
-
-    #       member_giver.reload
-    #       member_taker.reload
-    #       expect(member_giver.account.balance.to_i).to eq(-5)
-    #       expect(member_taker.account.balance.to_i).to eq(5)
-    #     end
-    #   end
-    # end
   end
 end
