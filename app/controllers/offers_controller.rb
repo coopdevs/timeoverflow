67d672bc941da88c95faa9ac785ceb08d0b07722
Patri <patrimglez@gmail.com>
2014-10-29 23:26:50 +0100

    sort the list of offers by category

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,7 +2,7 @@ class OffersController < PostsController
 
   def dashboard
     @offers = Hash.new
-    Category.all.each do |category|
+    Category.all.sort_by{ |a| a.name.downcase }.each do |category|
       offers_by_category = current_organization.offers.merge(category.posts).limit(5)
       @offers[category] = offers_by_category if offers_by_category.any?
     end
