a903ce3483901294810ec690558f47b50bcc68f8
Patri <patrimglez@gmail.com>
2014-11-17 20:40:45 +0100

    import users form csv

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,21 @@
 # coding: utf-8
 
 ActiveAdmin.register User do
+  action_item :only => :index do
+    link_to I18n.t("active_admin.users.upload_from_csv"), :action => 'upload_csv'
+  end
+
+  collection_action :upload_csv do
+    render "admin/csv/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    CsvDb.convert_save(params[:dump][:organization_id], params[:dump][:file])
+    redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
+
   index do
+    # selectable_column
     column do |user|
       link_to image_tag(avatar_url(user, 24)), admin_user_path(user)
     end
---
+++ app/models/csv_db.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+class CsvDb
+  class << self
+    def convert_save(organization_id, csv_data)
+      data = csv_data.read
+      data.force_encoding Encoding::ISO_8859_1
+      normalized_data = data.encode(Encoding::UTF_8)[1..-1]
+      CSV.parse(normalized_data, col_sep: ";", headers: false) do |row|
+        user = User.new(username: row[2..4].join(" "), date_of_birth: row[6], email: row[9], phone: row[7], alt_phone: row[8], gender: User::GENDERS[row[5].to_i - 1])
+        if user.save
+          member = Organization.find(organization_id).members.new(member_uid: row[0], entry_date: row[1], user_id: user_id)
+          member.save
+        else
+          # catch errors: user.errors.full_messages
+        end
+      end
+    end
+  end
+end
