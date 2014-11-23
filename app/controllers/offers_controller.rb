eb0a98fcf0f5cb02118f09714c7e71099a289157
Dani Macho <danielmacho72@gmail.com>
2014-11-23 11:22:33 +0100

    removed offers and inquiries link when not logged. Added new admin link to public link so organization can share in their websites

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,19 +1,23 @@
 class OffersController < PostsController
-  before_filter :has_organization, :only => 'dashboard'
+  #before_filter :has_organization, :only => 'dashboard'
 
   def dashboard
 
     @offers = Hash.new
     Category.all.sort_by{ |a| a.name.downcase }.each do |category|
-      offers_by_category = current_organization.offers.merge(category.posts).limit(5)
+      if current_organization
+        offers_by_category = current_organization.offers.merge(category.posts).limit(5)
+      else
+        offers_by_category = Offer.all.merge(category.posts).limit(5)
+      end
       @offers[category] = offers_by_category if offers_by_category.any?
     end
   end
 
   private
 
-  def has_organization
-     redirect_to(offers_path) unless current_organization
-  end
+  #def has_organization
+  #   redirect_to(offers_path) unless current_organization
+  #end
 
 end
