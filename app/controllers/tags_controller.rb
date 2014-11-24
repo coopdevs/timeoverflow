7b29ed36b75793cc70b7365c8de2cd8632766ce2
Saverio Trioni <saverio.trioni@gmail.com>
2014-11-24 05:42:05 +0100

    removed offers and inquiries link when not logged

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,62 +2,62 @@ class TagsController < ApplicationController
   respond_to :json, :html, :js
 
   def index
-
-    @all_tags = Post::find_like_tag(params[:term])
+    @posts = Post.by_organization(current_organization)
+    @all_tags = @posts.find_like_tag(params[:term])
     respond_with @all_tags
-
   end
 
   def alpha_grouped_index
-
     permitted = tags_params(params)
     post_type = permitted[:post_type] || "offer"
 
-    @offers_tagged=[]
-    @inquiries_tagged=[]
+    @offers_tagged = []
+    @inquiries_tagged = []
 
-    case post_type
-    when "offer"
-      @alpha_tags = Offer::alphabetical_grouped_tags
-      @current_post_type="offers"
-    when "inquiry"
-      @alpha_tags = Inquiry::alphabetical_grouped_tags
-      @current_post_type="inquiries"
-    when "all"
-      @alpha_tags = Post::alphabetical_grouped_tags
-      @current_post_type="all"
-    end
+    @alpha_tags = case post_type
+                  when "offer" then Offer
+                  when "inquiry" then Inquiry
+                  when "all" then Post
+                  end.by_organization(current_organization).
+                  alphabetical_grouped_tags
+
+    @current_post_type = case post_type
+                         when "offer" then "offers"
+                         when "inquiry" then "inquiries"
+                         when "all" then "all"
+                         end
 
     respond_with @alpha_tags
   end
 
   def inquiries
-    @current_post_type="inquiries"
-    @alpha_tags = Inquiry::alphabetical_grouped_tags
-    render :partial => 'grouped_index', :locals => { :alpha_tags => @alpha_tags }
+    @current_post_type = "inquiries"
+    @alpha_tags = Inquiry.by_organization(current_organization).
+                  alphabetical_grouped_tags
+    render partial: "grouped_index", locals: { alpha_tags: @alpha_tags }
   end
 
   def offers
-    @current_post_type="offers"
-    @alpha_tags = Offer::alphabetical_grouped_tags
-    render :partial => 'grouped_index', :locals => { :alpha_tags => @alpha_tags }
+    @current_post_type = "offers"
+    @alpha_tags = Offer.by_organization(current_organization).
+                  alphabetical_grouped_tags
+    render partial: "grouped_index", locals: { alpha_tags: @alpha_tags }
   end
 
   def posts_with
     permitted = tags_params(params)
     tagname = permitted[:tagname] || ""
 
-    @offers_tagged=Offer::tagged_with(tagname)
-    @inquiries_tagged=Inquiry::tagged_with(tagname)
-    #render :partial => 'tagged_index', :locals => { :offers_tagged => @offers_tagged,:inquiries_tagged => @inquiries_tagged }
-    respond_with @offers_tagged,@inquiries_tagged
+    @offers_tagged = Offer.by_organization(current_organization).
+                     tagged_with(tagname)
+    @inquiries_tagged = Inquiry.by_organization(current_organization).
+                        tagged_with(tagname)
+    respond_with @offers_tagged, @inquiries_tagged
   end
 
-
-
   private
+
   def tags_params(params)
     params.permit(:post_type, :tagname)
   end
-
 end
