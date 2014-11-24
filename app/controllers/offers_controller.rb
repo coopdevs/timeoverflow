6bf827602552f96aace6a7b4ce50bf41496142fd
Saverio Trioni <saverio.trioni@gmail.com>
2014-11-24 04:31:47 +0100

    respect hound happiness or it will bark at us

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,9 +4,8 @@ class OffersController < PostsController
   before_action :ensure_organization, only: :dashboard
 
   def dashboard
-    @offers = Category.all
-              .sort_by { |a| a.name.downcase }
-              .each_with_object({}) do |category, offers|
+    @offers = Category.all.sort_by { |a| a.name.downcase }.
+              each_with_object({}) do |category, offers|
       list = current_organization.offers.merge(category.posts).limit(5)
       offers[category] = list if list.present?
     end
