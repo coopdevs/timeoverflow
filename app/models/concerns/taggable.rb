be6d4a97e1cfacdf1c6756143f4c91beedd62c36
Saverio Trioni <saverio.trioni@gmail.com>
2014-11-24 02:23:12 +0100

    ended up rewriting completely the taggable file, so rubocop is happy

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -1,21 +1,27 @@
+# Taggable model concern
+#
+# Uses postgresql string arrays to provide tags without the additional tables.
+#
 module Taggable
   extend ActiveSupport::Concern
 
   included do
-    scope :tagged_with, ->(tag){ where("? = ANY (tags)", tag) }
-    scope :tagged_with_rank, ->(tag){ select("*,1 as rank").where("? = ANY (tags)", tag) }
+    scope :tagged_with, ->(tag) { where('? = ANY (tags)', tag) }
+    scope :tagged_with_rank, lambda { |tag|
+      select('*,1 as rank').where('? = ANY (tags)', tag)
+    }
   end
 
   def tag_list
-    tags && tags.join(", ")
+    tags && tags.join(', ')
   end
 
   def tag_list=(tag_list)
     self.tags = Array(tag_list.to_s.split(/,\s*/))
   end
 
+  # class methods (stupid comment to make rubocop happy)
   module ClassMethods
-
     def all_tags
       pluck(:tags).flatten.compact.reject(&:empty?)
     end
@@ -25,18 +31,15 @@ module Taggable
     end
 
     def tag_cloud
-      Hash[all_tags.group_by(&:to_s).values.map {|v| [v.first, v.size]}.sort]
+      Hash[all_tags.group_by(&:to_s).map { |k, v| [k, v.size] }.sort]
     end
 
     def find_like_tag(pattern)
-
-       all_tags.uniq.select{|t| t=~ /#{pattern}/}
-
+      all_tags.uniq.select { |t| t =~ /#{pattern}/ }
     end
 
     def alphabetical_grouped_tags
-      tag_cloud.group_by{ |l| l[0][0].capitalize }
+      tag_cloud.group_by { |l| l[0][0].capitalize }
     end
   end
-
 end
