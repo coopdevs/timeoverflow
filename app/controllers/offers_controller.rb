d3369e6afda7e55eacf307ca92543999460c3104
Saverio Trioni <saverio.trioni@gmail.com>
2014-11-24 05:30:08 +0100

    tag per company. fixed edit offers and inquiries errors. fixed rollbar issue #1371. fixed problem to show tags for active posts.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,19 +1,17 @@
 # Managems of offer-type posts
 #
 class OffersController < PostsController
-  before_action :ensure_organization, only: :dashboard
-
   def dashboard
+    initial_scope =
+      if current_organization
+        current_organization.offers
+      else
+        Offer.all
+      end
     @offers = Category.all.sort_by { |a| a.name.downcase }.
               each_with_object({}) do |category, offers|
-      list = current_organization.offers.merge(category.posts).limit(5)
+      list = initial_scope.merge(category.posts).limit(5)
       offers[category] = list if list.present?
     end
-  end
-
-  private
-
-  def ensure_organization
-    redirect_to(offers_path) unless current_organization
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
 
   def index
     @users = scoped_users
-    @memberships = current_organization.members.where(user_id: @users.map(&:id)).includes(:account).each_with_object({}) do |mem, ob|
-      ob[mem.user_id] = mem
-    end
+    @memberships = current_organization.members.
+                   where(user_id: @users.map(&:id)).includes(:account).
+                   each_with_object({}) do |mem, ob|
+                     ob[mem.user_id] = mem
+                   end
   end
 
   def show
@@ -28,31 +30,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    if @user = User.find_by_email(user_params[:email])
-      if !@user.active?
-        # Deactivated user is registered again (overwrite new attributes)
-        @user.attributes = user_params.merge(:active => true)
-        @user.save!
-      end
-    else
-      # New User
-      @user = User.new(user_params)
-      @user.skip_confirmation! #auto-confirm, not sending confirmation email
-      @user.save!
-    end
+    (@user = User.find_by_email(user_params[:email])) ? reactive_user : new_user
 
     if @user.persisted?
       @user.add_to_organization current_organization
-
       redirect_to users_path
     else
-      redirect_to :action => "new"
+      redirect_to action: "new"
     end
   end
 
   def update
-    @user = current_user if current_user.id == params[:id].to_i
-    @user ||= scoped_users.find(params[:id])
+    user_to_update
+
     if @user.update_attributes(user_params)
       respond_with @user, location: @user
     else
@@ -62,12 +52,16 @@ class UsersController < ApplicationController
 
   def give_time
     @user = scoped_users.find(params[:id])
-    @destination = @user.members.find_by(organization: current_organization).account.id
-    @source = current_user.members.find_by(organization: current_organization).account.id
-    @offer = current_organization.offers.find(params[:offer]) if params[:offer].present?
-    @transfer = Transfer.new(source: @source, destination: @destination, post: @offer)
+
+    get_transfer_data
+
+    @transfer = Transfer.new(source: @source,
+                             destination: @destination,
+                             post: @offer)
     if admin?
-      @sources = [current_organization.account] + current_organization.member_accounts.where("members.active is true")
+      @sources = [current_organization.account] +
+                 current_organization.member_accounts.
+                 where("members.active is true")
     end
   end
 
@@ -82,17 +76,48 @@ class UsersController < ApplicationController
       # TODO - Inquiries and Offers
     end
 
-    redirect_to :action => "index"
+    redirect_to action: "index"
   end
 
   private
 
   def user_params
-    fields_to_permit = %w"gender username email date_of_birth phone alt_phone active description"
-    fields_to_permit += %w"admin registration_number registration_date" if admin?
+    fields_to_permit = %w"gender username email date_of_birth phone alt_phone
+                          active description"
+    fields_to_permit += %w"admin registration_number
+                           registration_date" if admin?
     fields_to_permit += %w"organization_id superadmin" if superadmin?
     # params[:user].permit(*fields_to_permit).tap &method(:ap)
     params.require(:user).permit *fields_to_permit
   end
 
+  def new_user
+    # New User
+    @user = User.new(user_params)
+    @user.skip_confirmation! # auto-confirm, not sending confirmation email
+    @user.save!
+  end
+
+  def reactivate_user
+    if !@user.active?(current_organization)
+      # Deactivated user is registered again (overwrite new attributes)
+      @user.attributes = user_params.merge(active: true)
+      @user.save!
+    end
+  end
+
+  def user_to_update
+    @user = current_user if current_user.id == params[:id].to_i
+    @user ||= scoped_users.find(params[:id])
+  end
+
+  def get_transfer_data
+    @destination = @user.members.
+                   find_by(organization: current_organization).
+                   account.id
+    @source = current_user.members.
+              find_by(organization: current_organization).account.id
+    @offer = current_organization.offers.
+             find(params[:offer]) if params[:offer].present?
+  end
 end
