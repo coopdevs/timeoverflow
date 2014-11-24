896978597fecca11f8a7e6dd6b92ae126907a9c1
Saverio Trioni <saverio.trioni@gmail.com>
2014-11-24 03:41:44 +0100

    Revert "remove user from shared post action show in case no user logged in. "

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,19 +1,11 @@
 class OffersController < PostsController
-  before_filter :has_organization, :only => 'dashboard'
 
   def dashboard
-
     @offers = Hash.new
     Category.all.sort_by{ |a| a.name.downcase }.each do |category|
       offers_by_category = current_organization.offers.merge(category.posts).limit(5)
       @offers[category] = offers_by_category if offers_by_category.any?
     end
-  end
-
-  private
-
-  def has_organization
-     redirect_to(offers_path) unless current_organization
   end
 
 end
