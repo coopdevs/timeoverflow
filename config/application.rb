a51abf9c74ee4521630a2ead8fa22f4831e04b0d
Marc Anguera Insa <srmarc.ai@gmail.com>
2015-01-17 16:34:04 +0100

    move i18n settings to config/application.rb (default place in new apps); activate I18n fallbacks for all environments: restores previous behaviour

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Timeoverflow
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :ca, :en]
+    config.i18n.fallbacks = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
