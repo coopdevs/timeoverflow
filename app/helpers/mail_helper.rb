a80fb9ed4c447e8658b2299508bd31c706f7665c
Jesús Gutiérrez <jgutzm@gmail.com>
2017-01-19 18:30:07 +0100

    Add mail helper

---
+++ app/helpers/mail_helper.rb
@@ -0,0 +1,15 @@
+module MailHelper
+    def js_antispam_email_link(email,linktext)
+        # http://apidock.com/rails/ActionView/Helpers/UrlHelper/mail_to
+        user, domain = email.split('@')
+        # if linktext wasn't specified, throw email address builder into js document.write statement
+        linktext = "'+'#{user}'+'@'+'#{domain}'+'" if linktext == email 
+        out = "<script language='javascript'>\n"
+        out += "  <!--\n"
+        out += "    string = '#{user}'+'@'+''+'#{domain}';\n"
+        out += "    document.write('<a href='+'m'+'a'+'il'+'to:'+ string +'>#{linktext}</a>'); \n"
+        out += "  //-->\n"
+        out += "</script>\n"
+        return out        
+    end
+end
