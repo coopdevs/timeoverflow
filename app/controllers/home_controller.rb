bca8028834cd1e29058fd26bfba9431e7c4af376
Enrico Stano <ocirne.onats@gmail.com>
2017-06-15 23:35:20 +0200

    Redirect to first organization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+
+  # TODO: what happens when the user doesn't pertain to any organization?
   def index
-    redirect_to :users if current_user
+    return unless current_user
+
+    organization_id = current_user.organization_ids.first
+
+    redirect_to organization_path(id: organization_id)
   end
 end
