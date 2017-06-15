7b38ec550de8eb9f5f468346dcd9f4dac961fbb5
Enrico Stano <ocirne.onats@gmail.com>
2017-06-15 23:34:15 +0200

    Redirect to root after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
     if user.members.present?
-      users_path
+      root_path
     else
       page_path("about")
     end
