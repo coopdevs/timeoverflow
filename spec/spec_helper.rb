390cc4cae470b15762bed78e3336bbbd1b1dcb2c
Pau Perez <saulopefa@gmail.com>
2017-07-29 13:47:39 +0200

    Add first sign in feature specs
    This sets up capybara with the new headless chrome driver and adds the
    necessary requires in order to run single spec file. So far, there was
    no other way than doing `bundle exec rake` due to missing requires.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,25 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'database_cleaner'
 require 'fabrication'
+require 'selenium/webdriver'
 require 'faker'
 I18n.reload!
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
