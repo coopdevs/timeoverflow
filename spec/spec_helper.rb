5147b08bde7342bf46bafd8d13626fa48bf6a25c
Pau Perez <saulopefa@gmail.com>
2017-08-07 16:01:25 +0200

    Allow running a single spec file
    Due to missing requires the only way to execute unit tests is by doing
    `bundle exec rake`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,10 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
+require 'database_cleaner'
+require 'fabrication'
+require 'faker'
+I18n.reload!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
