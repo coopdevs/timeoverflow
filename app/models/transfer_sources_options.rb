5f4d70e79ce5a0188aa572ba2b99afc0872517a5
Pau Perez <saulopefa@gmail.com>
2018-01-04 12:04:40 +0100

    Add specs and docs to TransferSourceOptions

--- app/models/transfer_sources_options.rb
+++ app/models/transfer_sources_options.rb
@@ -1,9 +1,20 @@
+# Wraps the passed sources as a collection of HTML <option> tags displaying the
+# appropriate text for the :new transfer view
 class TransferSourcesOptions
+
+  # Constructor
+  #
+  # @param sources [Array<Account>]
+  # @param destination_accountable [Member | Organization]
   def initialize(sources, destination_accountable)
     @sources = sources
     @destination_accountable = destination_accountable
   end
 
+  # Returns the collection as an Array containing pairs of <option>'s text and
+  # value
+  #
+  # @return [Array<String, Integer>]
   def to_a
     sources
       .sort_by do |source|
