7b5184865ab9295e3bdd20ed47594d72fa924653
Pau Perez <saulopefa@gmail.com>
2018-01-04 16:26:58 +0100

    Inline AccountOptionTag to avoid wrong abstraction

--- app/models/transfer_sources_options.rb
+++ app/models/transfer_sources_options.rb
@@ -1,5 +1,5 @@
-# Wraps the passed sources as a collection of HTML <option> tags displaying the
-# appropriate text for the :new transfer view
+# Wraps the passed sources as a collection of HTML <option>'s value and text
+# pairs as expected by Rails' #options_for_select.
 class TransferSourcesOptions
 
   # Constructor
@@ -12,26 +12,33 @@ class TransferSourcesOptions
   end
 
   # Returns the collection as an Array containing pairs of <option>'s text and
-  # value
+  # value sorted by type and member_uid
   #
   # @return [Array<String, Integer>]
   def to_a
     sources
-      .sort_by do |source|
-      [
-        source.accountable_type,
-        source.accountable.try(:member_uid)
-      ]
-    end
-      .map do |account|
-      [
-        AccountOptionTag.new(account.accountable, destination_accountable).to_s,
-        account.id
-      ]
-    end
+      .sort_by { |account| to_accountable_type_and_uid(account) }
+      .map { |account| to_text_and_value(account) }
   end
 
   private
 
   attr_reader :sources, :destination_accountable
+
+  def to_accountable_type_and_uid(account)
+    [account.accountable_type, account.accountable.try(:member_uid)]
+  end
+
+  def to_text_and_value(account)
+    accountable = account.accountable
+
+    [
+      "#{display_id(accountable)} #{accountable.class} #{accountable}",
+      account.id
+    ]
+  end
+
+  def display_id(accountable)
+    accountable.display_id(destination_accountable)
+  end
 end
