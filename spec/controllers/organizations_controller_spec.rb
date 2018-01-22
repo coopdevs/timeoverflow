e40a0ff75f3a25943ad4fbb7a5685d49b2f4b8a0
Pau Perez <saulopefa@gmail.com>
2018-01-22 15:29:12 +0100

    Replace give_time link from organizations#show

---
+++ spec/controllers/organizations_controller_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe OrganizationsController do
+  describe '#show' do
+    let(:organization) { Fabricate(:organization) }
+
+    it 'links to new_transfer_path' do
+      get 'show', id: organization.id
+      expect(response.body).to include(
+        "<a href=\"/transfers/new?destination_account_id=#{organization.account.id}&amp;id=#{organization.id}\">"
+      )
+    end
+  end
+end
