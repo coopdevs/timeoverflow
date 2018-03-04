1512cd34a773274395b52cc70a3fcc39bad4cd96
Jorge Morante <jorge@morante.eu>
2018-03-04 21:05:40 +0100

    wtf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,7 @@ Capybara.register_driver :headless_chrome do |app|
 end
 
 Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :headless_chrome
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
