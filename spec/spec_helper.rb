c4e3db4e1ae5e6a5d3cc12bc543385dfea8929a6
Jorge Morante <jorge@morante.eu>
2018-03-18 20:23:49 +0100

    fix database cleaner conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -79,22 +79,26 @@ RSpec.configure do |config|
     end
   end
 
-  # Database cleaner configuration
+  config.use_transactional_fixtures = false
+
   config.before(:suite) do
     if config.use_transactional_fixtures?
       raise(<<-MSG)
-        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
-        (or set it to false) to prevent uncommitted transactions being used in
-        JavaScript-dependent specs.
+      Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+      (or set it to false) to prevent uncommitted transactions being used in
+      JavaScript-dependent specs.
 
-        During testing, the app-under-test that the browser driver connects to
-        uses a different database connection to the database connection used by
-        the spec. The app's database connection would not be able to access
-        uncommitted transaction data setup over the spec's database connection.
+      During testing, the app-under-test that the browser driver connects to
+      uses a different database connection to the database connection used by
+      the spec. The app's database connection would not be able to access
+      uncommitted transaction data setup over the spec's database connection.
       MSG
     end
-
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each, type: :feature) do
@@ -108,10 +112,6 @@ RSpec.configure do |config|
       # specs, so use truncation strategy.
       DatabaseCleaner.strategy = :truncation
     end
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each) do
