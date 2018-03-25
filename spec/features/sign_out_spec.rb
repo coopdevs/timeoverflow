90ac1d456bfa6e623a70d7392d273f9f9dd563d4
Jorge Morante <jorge@morante.eu>
2018-03-25 15:27:00 +0200

    fix sign out spec

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 feature 'sign out' do
   let!(:user) do
-    Fabricate(:user, email: 'user@timeoverflow.org', password: 'papapa22')
+    Fabricate(
+      :user,
+      email: 'user@timeoverflow.org',
+      password: 'papapa22',
+      terms_accepted_at: 1.day.from_now
+    )
   end
 
   it 'signs the user out' do
     sign_in_with(user.email, user.password)
+    click_link user.email
     click_link I18n.t('application.navbar.sign_out')
 
     expect(current_path).to eq(root_path)
