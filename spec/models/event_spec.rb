0da31cbcb7573b3ee24e6d09607e2f908f736c97
Enrico Stano <ocirne.onats@gmail.com>
2018-05-10 22:35:10 +0200

    Add more specs to Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,25 +1,46 @@
 require "spec_helper"
 
 describe Event do
-  let(:post) { Fabricate(:post) }
-  let(:member) { Fabricate(:member) }
-  let(:event) { Event.new action: 'create' }
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:action) }
+    it { is_expected.to validate_inclusion_of(:action).in_array(::Event::ACTIONS) }
 
-  describe '#resource_presence validation' do
-    context 'has no resources' do
-      it { expect(event).to_not be_valid }
+    describe '#resource_presence validation' do
+      let(:post) { Fabricate(:post) }
+      let(:member) { Fabricate(:member) }
+      let(:event) { Event.new action: 'create' }
+
+      context 'has no resources' do
+        it { expect(event).to_not be_valid }
+      end
+
+      context 'has one resource' do
+        before { event.post = post }
+
+        it { expect(event).to be_valid }
+      end
+
+      context 'has two resources' do
+        before { event.post = post; event.member = member }
+
+        it { expect(event).to_not be_valid }
+      end
     end
+  end
 
-    context 'has one resource' do
-      before { event.post = post }
+  describe 'Relations' do
+    it { is_expected.to belong_to(:post) }
+    it { is_expected.to belong_to(:member) }
+    it { is_expected.to belong_to(:transfer) }
 
-      it { expect(event).to be_valid }
-    end
+    it { is_expected.to have_db_column(:post_id) }
+    it { is_expected.to have_db_column(:member_id) }
+    it { is_expected.to have_db_column(:transfer_id) }
+  end
 
-    context 'has two resources' do
-      before { event.post = post; event.member = member }
-
-      it { expect(event).to_not be_valid }
-    end
+  describe 'Indexes' do
+    it { is_expected.to have_db_index(:post_id) }
+    it { is_expected.to have_db_index(:member_id) }
+    it { is_expected.to have_db_index(:transfer_id) }
   end
 end
