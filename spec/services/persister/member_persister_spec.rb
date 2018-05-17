dabf7e4e3ea8370fd4b5c779d5e9e70cf7867a30
Enrico Stano <ocirne.onats@gmail.com>
2018-05-17 09:18:26 +0200

    Create :created and :updated event for Member

--- spec/services/persister/member_persister_spec.rb
+++ spec/services/persister/member_persister_spec.rb
@@ -3,15 +3,32 @@ require 'spec_helper'
 describe Persister::MemberPersister do
   let(:organization) { Fabricate(:organization) }
   let(:user) { Fabricate(:user) }
+  let(:member) { Fabricate.build(:member, user: user, organization: organization) }
+  let(:persister) { ::Persister::MemberPersister.new(member) }
 
   describe '#save' do
+    before { persister.save }
+
     it 'saves the member' do
-      member = Member.new(user: user, organization: organization)
-
-      persister = ::Persister::MemberPersister.new(member)
-      persister.save
-
       expect(member).to be_persisted
+    end
+
+    # TODO: write better expectation
+    it 'creates an event' do
+      expect(Event.where(member_id: member.id).first.action).to eq('created')
+    end
+  end
+
+  describe '#update_attributes' do
+    before { persister.update_attributes(member_uid: 666) }
+
+    it 'updates the resource attributes' do
+      expect(member.member_uid).to eq(666)
+    end
+
+    # TODO: write better expectation
+    it 'creates an event' do
+      expect(Event.where(member_id: member.id).first.action).to eq('updated')
     end
   end
 end
