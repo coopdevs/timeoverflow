833bf307b6117cb7f1707225a8406d0cffb9c406
Enrico Stano <ocirne.onats@gmail.com>
2018-05-17 08:53:33 +0200

    Create :updated event on Post update

--- app/services/persister/post_persister.rb
+++ app/services/persister/post_persister.rb
@@ -17,13 +17,23 @@ module Persister
     end
 
     def update_attributes(params)
-      post.update_attributes(params)
+      ::ActiveRecord::Base.transaction do
+        post.update_attributes!(params)
+        create_update_event!
+        post
+      end
+    rescue ActiveRecord::RecordInvalid => _exception
+      false
     end
 
     private
 
     def create_save_event!
       ::Event.create! action: :created, post: post
+    end
+
+    def create_update_event!
+      ::Event.create! action: :updated, post: post
     end
   end
 end
