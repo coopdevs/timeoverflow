4a318ec728e0d0e6a7d03a74e92e78061a24c67e
Maxim Colls <collsmaxim@gmail.com>
2018-06-13 19:38:06 +0100

    Remember users for 4 weeks if they check the option

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -127,7 +127,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  config.remember_for = 2.weeks
+  config.remember_for = 4.weeks
 
   # If true, extends the user's remember period when remembered via cookie.
   config.extend_remember_period = false
