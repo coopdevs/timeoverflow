99bc227e4c00ea7695451ada5644cc8676c6e093
Jordi Sabaté <zarpa37@gmail.com>
2019-06-20 21:50:16 +0200

    test reverted

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,8 +7,6 @@ RSpec.feature 'sign in' do
 
   context 'with a valid password' do
     it 'signs the user in' do
-      visit('/')
-
       expect(Capybara.current_session.driver.browser.manage.cookie_named('_timeoverflow_session')).to be_falsy
 
       sign_in_with(user.email, user.password)
@@ -20,7 +18,6 @@ RSpec.feature 'sign in' do
 
   context 'with an invalid password' do
     it 'shows an error' do
-      visit('/')
       sign_in_with(user.email, 'wrong_password')
       expect(page).to have_content(I18n.t('devise.failure.invalid'))
     end
