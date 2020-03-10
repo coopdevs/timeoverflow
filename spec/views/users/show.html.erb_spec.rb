1eb9cb9ad855d1357f2e2910d251954c4ffbfd76
marisa <mperez18@jaumebalmes.net>
2020-03-10 10:39:44 +0100

    WIP - the test doesn't print the link

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -33,31 +33,39 @@ RSpec.describe 'users/show' do
     let(:destination_account) { Fabricate(:account) }
 
     let!(:user) { member.user }
-    let!(:another_user) { another_member.user }
     let!(:admin_user) { member_admin.user }
-    let!(:wrong_user) { wrong_email_member.user }
-    let!(:empty_email_user) { empty_email_member.user }
 
 
     let(:logged_user) { Fabricate(:user) }
+
     before do
         allow(view).to receive(:admin?).and_return(false)
     end
     
 
     it 'renders a link to new_transfer_path for their individual offers' do
-        assign :offer, offer
         assign :destination_account, destination_account
+        assign :user, user
+        assign :member, member
+        assign :offer, offer
+        assign :offers, member.user.offers.active
+        assign :movements, member.movements.page
+
         allow(view).to receive(:current_user).and_return(logged_user)
+        
         render template: 'users/show'
 
+        #expect(rendered).to have_selector("//a", text: "/transfers/new?destination_account_id=#{member.account.id}&amp;id=#{user.id}&amp;offer=#{offer.id}")
+        
+        byebug
         expect(rendered).to have_link(
-            t('users.show.give_time'),
+            nil,
             href: new_transfer_path(
                 id: user.id,
-                destination_account_id: destination_account.id,
+                destination_account_id: member.account.id,
                 offer: offer.id
             )
-        )
-    end      
+        ) 
+            
+     end
 end
