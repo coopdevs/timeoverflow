d4001d03e05ea6c9c0b39129e158cfd0311bf292
Marc Anguera Insa <srmarc.ai@gmail.com>
2020-12-17 23:26:36 +0100

    travis: try using psql directly; make codeclimate happy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,18 +2,24 @@
 
 $(document).on('click', 'a[data-popup]', function(event) {
   event.preventDefault();
+
   window.open($(this).attr('href'), 'popup', 'width=600,height=600');
 });
 
 $(document).on('click', 'span.show-password', function(event) {
   event.preventDefault();
+
+  var inputType = 'text';
+  var icon = 'visibility_off';
+
   if ($(this).hasClass('checked')) {
     $(this).removeClass('checked');
-    $(this).prev('input').attr('type', 'password');
-    $(this).find('.material-icons').html("visibility");
+    inputType = 'password';
+    icon = 'visibility';
   } else {
     $(this).addClass('checked');
-    $(this).prev('input').attr('type', 'text');
-    $(this).find('.material-icons').html("visibility_off");
   }
+
+  $(this).prev('input').attr('type', inputType);
+  $(this).find('.material-icons').html(icon);
 });
