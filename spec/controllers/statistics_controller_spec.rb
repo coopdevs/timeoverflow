f92b6537d035b2679726902e8aec3453093ccd7b
Marc Anguera Insa <srmarc.ai@gmail.com>
2021-03-13 20:09:35 +0100

    statistics code clean up

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe StatisticsController do
 
   before(:each) { login(member.user) }
 
-  describe '#statistics_all_transfers' do
+  describe '#all_transfers' do
     it 'populates all transfers from current organization' do
       transfer = Fabricate(:transfer, source: organization.account, destination: member.account)
       transfer2 = Fabricate(:transfer)
 
-      get :statistics_all_transfers
+      get :all_transfers
 
       expect(assigns(:transfers)).to eq([transfer])
     end
