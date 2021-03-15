84eb627920aad8a03fb5fc11ce1902a635bdecfe
Marc Anguera Insa <srmarc.ai@gmail.com>
2021-03-15 23:57:45 +0100

    Some Admin improvements:
    - Rename and refactor CsvDb => UserImporter + add specs (I have resurrected an old branch from Pau: https://github.com/coopdevs/timeoverflow/compare/improve-csv-user-import)
    - ActiveAdmin settings clean up. This file is very long and always out of date, better to check docs online ;p
    - ActiveAdmin improve menu: add current user + add all available locales
    
    Co-authored-by: Pau Pérez Fabregat <saulopefa@gmail.com>

---
+++ spec/services/user_importer_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe UserImporter do
+  describe '#call' do
+    let(:organization) { Fabricate(:organization) }
+    let(:user) do
+      User.new(
+        username: 'Hermione Cadena Muñoz',
+        date_of_birth: '1989-03-16',
+        email: 'user@example.com',
+        phone: '622743103',
+        alt_phone: '691777984',
+        gender: 'female'
+      )
+    end
+
+    # member_id, entry_date, username, gender, date_of_birth, phone, alt_phone, email
+    let(:csv_data) { StringIO.new('1,2018-01-30,Hermione,Cadena,Muñoz,1,1989-03-16,622743103,691777984,user@example.com') }
+
+    before do
+      allow(Organization)
+        .to receive(:find).with(organization.id).and_return(organization)
+
+      allow(User).to receive(:new).with(
+        username: 'Hermione Cadena Muñoz',
+        date_of_birth: '1989-03-16',
+        email: 'user@example.com',
+        phone: '622743103',
+        alt_phone: '691777984',
+        gender: 'female'
+      ).and_return(user)
+    end
+
+    it 'creates a user out of a CSV row' do
+      expect(User).to receive(:new).with(
+        username: 'Hermione Cadena Muñoz',
+        date_of_birth: '1989-03-16',
+        email: 'user@example.com',
+        phone: '622743103',
+        alt_phone: '691777984',
+        gender: 'female'
+      ).and_return(user)
+
+      described_class.call(organization.id, csv_data)
+    end
+
+    it 'persists the user' do
+      expect(user).to receive(:save)
+      described_class.call(organization.id, csv_data)
+    end
+  end
+end
