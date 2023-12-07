78d0fb001532507826fedb3def4fe17d2e7c17f1
Nicolas Florentin <nicolas@sleede.com>
2023-12-07 09:33:56 +0100

    makes member searches accent insensitive

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,8 @@
+Ransack.configure do |config|
+  config.add_predicate 'unaccent_cont',
+                       arel_predicate: 'matches',
+                       formatter: proc { |s| ActiveSupport::Inflector.transliterate("%#{s}%") },
+                       validator: proc { |s| s.present? },
+                       compounds: true,
+                       type: :string
+end
