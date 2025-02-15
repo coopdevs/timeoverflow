ff0102e1f46331ce67999808fa08e382c06be668
Marc Anguera Insa <srmarc.ai@gmail.com>
2025-02-15 16:39:47 +0100

    remove useless method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,10 +48,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # https://www.relishapp.com/rspec/rspec-rails/docs/upgrade#pending-migration-checks
 ActiveRecord::Migration.maintain_test_schema!
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
 RSpec.configure do |config|
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
