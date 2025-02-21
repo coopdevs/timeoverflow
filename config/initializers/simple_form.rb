b16111aa7c19e2112fe60a389a23704c682d0a6c
Fran Perejón Barrios <franperejonbarrios@gmail.com>
2025-02-21 22:20:36 +0100

    changes up to form-group

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -95,7 +95,7 @@ SimpleForm.setup do |config|
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
 
   # You can define the class to use on all labels. Default is nil.
-  config.label_class = 'control-label'
+  config.label_class = 'col-form-label'
 
   # You can define the class to use on all forms. Default is simple_form.
   # config.form_class = :simple_form
