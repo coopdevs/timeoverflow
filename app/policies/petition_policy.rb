b1702c2cf5b9b25ce397b92c717e75050c4b17a6
Marc Anguera Insa <srmarc.ai@gmail.com>
2025-03-23 17:11:19 +0100

    some times there is no member (@user = member.user)

--- app/policies/petition_policy.rb
+++ app/policies/petition_policy.rb
@@ -2,10 +2,10 @@ class PetitionPolicy < ApplicationPolicy
   alias_method :petition, :record
 
   def update?
-    user.superadmin? || user.admins?(petition.organization)
+    user&.superadmin? || user&.admins?(petition.organization)
   end
 
   def manage?
-    user.superadmin? || user.admins?(organization)
+    user&.superadmin? || user&.admins?(organization)
   end
 end
